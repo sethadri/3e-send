2bb7bb0f0f3bacd0814e8ba0bfc783c0040c7b67
sethadri <102585785+sethadri@users.noreply.github.com>
2026-05-31 10:14:30 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 
+function calc(){
+ let amt=parseFloat(document.getElementById('amount').value||0);
+ document.getElementById('result').innerText='Recipient Gets: '+(amt*17.85).toFixed(2);
+}
+document.getElementById('amount').addEventListener('input',calc);
